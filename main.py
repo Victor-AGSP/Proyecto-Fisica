d649dd6a6119e83b224588c032ad2c176c0ad9c2
Victor <victorsepulveda.agp@gmail.com>
2023-06-03 20:10:46 -0400

    Script Principal
    Manejo de ventana principal

---
+++ main.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+# Crear Ventana
+ventana = tk.Tk()
+
+# Título de la ventana
+ventana.title("Energía y trabajo de la fuerza de roce (Sistema no conservativo")
+
+# Tamaño de la ventana
+ancho = 400
+alto = 300
+ventana.geometry(f"{ancho}x{alto}")
+
+# Fijar tamaño de la ventana
+ventana.resizable(False, False)
+
+# Mantener ventana permanente
+ventana.mainloop()
